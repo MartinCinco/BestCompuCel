d337bc6d0a10ce1350a6bbcef9358c809f206eee
Martin Cinco Ruiz <jet5boymartin@gmail.com>
2023-07-04 17:13:20 -0900

    ianeedlan

--- BestCompuCel/BestCompuCel/Formularios/Form1.Designer.cs
+++ BestCompuCel/BestCompuCel/Formularios/Form1.Designer.cs
@@ -67,6 +67,7 @@
             this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.lbRuta = new System.Windows.Forms.Label();
             this.lbAviso = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
@@ -393,15 +394,25 @@
             this.lbAviso.AutoSize = true;
             this.lbAviso.Location = new System.Drawing.Point(27, 417);
             this.lbAviso.Name = "lbAviso";
-            this.lbAviso.Size = new System.Drawing.Size(35, 13);
+            this.lbAviso.Size = new System.Drawing.Size(0, 13);
             this.lbAviso.TabIndex = 36;
-            this.lbAviso.Text = "label5";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(293, 417);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(186, 31);
+            this.label5.TabIndex = 37;
+            this.label5.Text = "algobien loco";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 640);
+            this.Controls.Add(this.label5);
             this.Controls.Add(this.lbAviso);
             this.Controls.Add(this.lbRuta);
             this.Controls.Add(this.button1);
@@ -486,6 +497,7 @@
         private System.Windows.Forms.Timer timer1;
         private System.Windows.Forms.Label lbRuta;
         private System.Windows.Forms.Label lbAviso;
+        private System.Windows.Forms.Label label5;
     }
 }
 
